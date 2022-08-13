6aae5f1c1478a689d2e101ba058e6df805ed7e00
Niladree Datta <62564128+Niladree60@users.noreply.github.com>
2022-08-13 15:48:00 +0600

    Create Parenthesis Matching Using Stack Without STL.c

---
+++ Parenthesis Matching Using Stack Without STL.c
@@ -0,0 +1,134 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct stack
+{
+    int size;
+    int top;
+    char *arr; // Array in Heap
+
+};
+void display(struct stack *  ptr){
+    for (int i = 0; i < ptr->size; i++)
+    {
+        printf("%d\n",ptr->arr[i]);
+    }
+
+}
+int isEmpty(struct stack * ptr)
+{
+    // O(1)
+    if(ptr -> top == -1)
+    {
+        return 1;
+    }
+    else return 0;
+
+}
+int isFull(struct stack * ptr)
+{
+    // O(1)
+    if(ptr -> top == ptr -> size -1)
+    {
+        return 1;
+    }
+    else return 0;
+
+}
+void push ( struct stack * ptr, char val)
+{
+    // O(1)
+    if(isFull(ptr))
+    {
+        printf("Stack Overflow\n");
+    }
+    else
+    {
+        ptr -> top++;
+        ptr -> arr[ptr->top] = val;
+    }
+}
+char pop ( struct stack * ptr)
+{
+    // O(1)
+    if(isEmpty(ptr))
+    {
+        printf("Stack Underflow\n");
+        return -1;
+    }
+    else
+    {
+        char val = ptr->arr[ptr->top];
+        ptr->top--;
+        return val;
+    }
+}
+int peek(struct stack * sp, int i)
+{
+    // O(1)
+    int indx = sp -> top-i+1;
+    if(indx < 0)
+    {
+        printf("Not a Valid Position\n");
+        return -1;
+    }
+    else
+    {
+        return sp->arr[indx];
+
+    }
+}
+int stackTop(struct stack * sp)
+{
+    return sp->arr[sp->top];
+}
+int stackBottom(struct stack * sp)
+{
+    return sp->arr[0];
+}
+
+int parenthesisMatch(char * exp)
+{
+    // create and initialize the stack
+    struct stack * sp;
+    sp->size = 100;
+    sp->top = -1;
+    sp->arr = (char *)malloc(sp -> size * sizeof(char));
+    for(int i =0  ; exp[i] != '\0' ; i++)
+    {
+        if(exp[i] == '(')
+        {
+            push(sp,'(');
+
+        }
+        else if(exp[i] == ')')
+        {
+            if(isEmpty(sp))
+            {
+                return 0;
+            }
+            pop(sp);
+
+        }
+    }
+    if(isEmpty(sp))
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int main()
+{
+    char * exp = "((8)*(9))";
+    if(parenthesisMatch(exp))
+    {
+        printf("Parenthesis Matching");
+    }
+    else printf("Parenthesis Not Matching");
+
+
+    return 0;
+}
